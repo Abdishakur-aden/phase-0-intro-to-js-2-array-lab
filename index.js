693dda40cbdbcf2e5bdc4cfceb30b19c5bce4908
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-23 12:51:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat (name){
+    cats.push(name);
+}
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyPrependCat (name){
+    cats.unshift(name);
+}
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function appendCat (name) {
+    return [...cats, name];    
+}
+
+function prependCat (name){
+    return [name, ...cats]
+}
+
+function removeLastCat (){
+    return [...cats.slice(0, 2)];
+}
+
+function removeFirstCat (){
+    return [...cats.slice(1, 3)]
+}
